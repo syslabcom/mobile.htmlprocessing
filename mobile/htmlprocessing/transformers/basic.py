b006e4f31e6898fdfec8d0620b1280ad2aa24524
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2010-11-05 13:33:46 +0000

    Allow start with bad lxml

--- mobile/htmlprocessing/transformers/basic.py
+++ mobile/htmlprocessing/transformers/basic.py
@@ -26,72 +26,83 @@ try:
     from lxml.html import _nons, _transform_result
     
     from lxml.html.clean import Cleaner
+    
+    LXML = True
 
 except ImportError, e:
     #raise RuntimeError("Please install lxml: http://pypi.python.org/pypi/lxml/")
     logger.exception(e)
     logger.error("Could not import lxml - probably not installed or too old version")
     
+    LXML = False
+    
+
+if LXML:
+
+    class BasicCleaner(Cleaner):
+        """ Clean incoming HTML to be valid XHTML mobile profile without any nastiness """
+    
+        def __init__(self, trusted=False):
+            """ Initialize HTML cleaner.
+            
+            Set whether we want to filter out nasty tags also.
+             
+            @param trusted: True if HTML is from trusted source 
+            """
+            self.trusted = trusted
+            
+        def add_alt_tags(self, el):
+            """ Alt tags are needed for every image.
+    
+            If not ALT tag is present, put in an empty string.
+            """
+            if not "alt" in el.attrib:
+                el.attrib["alt"] = ""
+    
+        def process_img(self, doc, el):
+            self.add_alt_tags(el)
+    
+        def process_imgs(self, doc):
+            for el in doc.iter('img'):
+                self.process_img(doc, el)
+    
+        def clean_mobile(self, doc):
+            """ Run XHTML-MP specific cleaners for the document.
+            """
+            self.process_imgs(doc)
+    
+        def process(self, html):
+            """ Run XHTML mobile profile cleaner for HTML code.
+    
+            @param html: HTML as a strinrg or lxml Document
+            @return: XHTML, utf-8 encoded string
+            """
+            
+            # Check whether we got ready parse-tree or string input
+            result_type = type(html)
+    
+            if isinstance(html, basestring):
+                doc = fromstring(html)
+            else:
+                doc = copy.deepcopy(html)
+    
+            # Run XHTML MP specific cleaning
+            self.clean_mobile(doc)
+    
+            # Run normal cleaning
+            if not self.trusted:
+                self(doc)
+    
+            return tostring(doc, method="xml", encoding='UTF-8')
+
+else:
+    # No LXML
     class BasicCleaner(object):
         def __init__(self, trusted=False):
             pass
         
-
-class BasicCleaner(Cleaner):
-    """ Clean incoming HTML to be valid XHTML mobile profile without any nastiness """
-
-    def __init__(self, trusted=False):
-        """ Initialize HTML cleaner.
-        
-        Set whether we want to filter out nasty tags also.
-         
-        @param trusted: True if HTML is from trusted source 
-        """
-        self.trusted = trusted
-        
-    def add_alt_tags(self, el):
-        """ Alt tags are needed for every image.
-
-        If not ALT tag is present, put in an empty string.
-        """
-        if not "alt" in el.attrib:
-            el.attrib["alt"] = ""
-
-    def process_img(self, doc, el):
-        self.add_alt_tags(el)
-
-    def process_imgs(self, doc):
-        for el in doc.iter('img'):
-            self.process_img(doc, el)
-
-    def clean_mobile(self, doc):
-        """ Run XHTML-MP specific cleaners for the document.
-        """
-        self.process_imgs(doc)
-
-    def process(self, html):
-        """ Run XHTML mobile profile cleaner for HTML code.
-
-        @param html: HTML as a strinrg or lxml Document
-        @return: XHTML, utf-8 encoded string
-        """
-        
-        # Check whether we got ready parse-tree or string input
-        result_type = type(html)
-
-        if isinstance(html, basestring):
-            doc = fromstring(html)
-        else:
-            doc = copy.deepcopy(html)
-
-        # Run XHTML MP specific cleaning
-        self.clean_mobile(doc)
-
-        # Run normal cleaning
-        if not self.trusted:
-            self(doc)
-
-        return tostring(doc, method="xml", encoding='UTF-8')
+        def process(self, html):
+            raise NotImplementedError("lxml support needed")
 
 cleaner = BasicCleaner()
 
